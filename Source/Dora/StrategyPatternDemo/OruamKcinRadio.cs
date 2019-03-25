49aaf3fc09af607be111f60bedb33865fa782408
Nick <nmauro@avtecinc.com>
2019-03-25 08:39:03 -0400

    Added radio

---
+++ Source/Dora/StrategyPatternDemo/OruamKcinRadio.cs
@@ -0,0 +1,10 @@
+﻿namespace Avtec.DevMorningFix.Dora.StrategyPatternDemo
+{
+    internal class OruamKcinRadio : AnalogRadio
+    {
+        public override void Transmit(ITransmitReceiver output)
+        {
+            output.TransmitData("Lift heavy, eat tacos.");
+        }
+    }
+}
