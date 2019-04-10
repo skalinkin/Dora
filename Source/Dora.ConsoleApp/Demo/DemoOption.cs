110643911130113c224e7e5eeb30cf150b742e43
Yackey <syackey-qa@qa.avtecinc.com>
2019-04-10 08:25:34 -0400

    added command line usage example

--- Source/Dora.ConsoleApp/Demo/DemoOption.cs
+++ Source/Dora.ConsoleApp/Demo/DemoOption.cs
@@ -6,7 +6,7 @@ namespace Avtec.DevMorningFix.Dora.ConsoleApp.Demo
     [Verb("demo", HelpText = "Demonstrate application of a pattern")]
     internal class DemoOption : IOption
     {
-        [Option('p', "pattern", Required = true)]
+        [Option('p', "pattern", Required = true, HelpText = "For example, demo -p Strategy or demo --pattern Strategy. The -p value is CaSe sensitive.")]
         public Demos Pattern { get; set; }
 
         public Type GetCommandType()
